3254bdcadf760eb841097ca6d2dd115733dfe286
Mehmet Coşkun <mehmet.coskun@windowslive.com>
2022-06-10 00:52:11 +0300

    Tanitim temasi

---
+++ tesekkurler.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Coronals</title>
+	<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    
+    
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script src="js/wow.min.js"></script>
+
+
+	
+    		
+  </head>
+  <body>
+
+
+  
+    
+<video  autoplay loop mute> 
+
+<source src="vids/river.mp4" type="video/mp4"> 
+
+  </video>    
+
+<div  class="container-fluid ust ">
+
+		<div class="row">
+            
+            	<div class="col-md-4 col-xs-12 logo">
+                
+                	<a href="index.html"><img src="img/logo.png"></a>
+                
+                </div>
+                
+                <div class="col-md-8 col-xs-12 menu">
+                
+               		
+
+<div class="menuAc">Menü</div>
+	<ul>
+    	<li><a href="index.html" ><span class="glyphicon glyphicon-home" aria-hidden="true"></span></a></li>
+		<li><a href="index.html#hakkimizda" >BiZi TANIYIN</a></li>
+        <li><a href="index.html#ortaklar" >EKiP</a></li>
+        <li><a href="index.html#yaptiklar" >NELER YAPTIK ?</a></li>
+		<li><a href="index.html#hizmetler" >NELER YAPIYORUZ ?</a></li>
+		<li><a href="index.html#iletisim" >iLETiSiM</a></li>
+	</ul>
+    
+				
+                </div>
+                
+                
+        
+            </div>
+        
+</div>
+
+<div class="container-fluid tesekkur ">
+   <div class="row">
+   <div class="col-md-12 we-do-grid wow bounceIn" data-wow-delay="0.4s">
+    		
+ 			<img src="img/ok.png">
+            <h4>Bilgileriniz Ekibimize iletildi En Kısa Sürede Dönüs Saglanacaktır. Tesekkürler.</h4>
+            
+</div>
+            </div>
+    </div>
+
+		
+    
+    
+    
+    
+   <div class="container-fluid footer ">
+       	<div class="row">
+        <div class="col-md-12 design">
+        
+        <img src="img/design.png" >
+        
+        </div>
+        
+        </div>     
+        <div class="row">
+        <div class="col-md-12 footerikon">
+        <a href="#"><img src="img/ikon/facebook.png" width="45" height="45"></a>
+        <a href="#"><img src="img/ikon/googleplus.png" width="45" height="45"></a>
+        <a href="#"><img src="img/ikon/youtube.png" width="45" height="45"></a>
+        <a href="#"><img src="img/ikon/twitter.png" width="45" height="45"></a>
+        <a href="#"><img src="img/ikon/linkedin.png" width="45" height="45"></a>
+        </div>
+        </div>
+           <div class="row ">
+        <div class="col-md-12 footeryazi">
+        Coronals Yazılım ve Danışmanlık &copy Tüm Hakları Sakldır
+        </div>
+        </div>
+        </div>     
+ 
+
+
+
+ 
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+ 
+        
+	
+
+	<script type="text/javascript">
+	$(function(){
+		var a = 0;
+		$('.menuAc').click(function(){
+			if ( a == 0 ){
+				$(this).text('Menü');
+				a++;
+			} else {
+				$(this).text('Menü');
+				a = 0;
+			}
+			$(this).next('ul').slideToggle(500);
+		});
+	});
+
+
+</script>
+
+	
+
+  </body>
+</html>
+
